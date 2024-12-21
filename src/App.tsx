cdd7f3b4da0a9ba5df3f47788408d70628eaac74
AlvisonHunterArnuero <alvison@gmail.com>
2024-12-21 02:24:42 -0600

    refactor(app): added body class rules & incorporated header & footer comps

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useContext, useEffect } from 'react';
 import DataContext from './DataProviderContext';
 
 import Card from './components/Card';
+import Header from './components/Heaader.tsx';
+import Footer from './components/Footer.tsx';
 
 function App() {
   const { data, fetchData } = useContext(DataContext);
@@ -11,28 +13,32 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-wrap min-h-screen gap-4 p-4">
-      {data.map((item) => {
-        const {
-          id,
-          title,
-          excerpt,
-          date,
-          jetpack_featured_media_url,
-        } = item;
-        return (
-          <Card
-            key={item.id}
-            cardData={{
-              id,
-              title,
-              excerpt,
-              date,
-              jetpack_featured_media_url,
-            }}
-          />
-        );
-      })}
+    <div className="flex flex-col w-screen max-w-screen-xl">
+      <Header />
+      <div className="flex p-2 h-screen w-screen max-screen-xl flex-wrap">
+        {data.map((item) => {
+          const {
+            id,
+            title,
+            excerpt,
+            date,
+            jetpack_featured_media_url,
+          } = item;
+          return (
+            <Card
+              key={item.id}
+              cardData={{
+                id,
+                title,
+                excerpt,
+                date,
+                jetpack_featured_media_url,
+              }}
+            />
+          );
+        })}
+      </div>
+      <Footer />
     </div>
   );
 }
